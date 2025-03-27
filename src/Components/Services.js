3d15e4d19db75d94d8c347917e7a6ae631b13dc5
ilimKUSH <ikusbekov@gmail.com>
2025-03-27 10:44:47 +0600

    add: about

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,64 +1,88 @@
-import React from 'react'
-import {Container, Image} from 'react-bootstrap'
-import {v4 as uuid} from 'uuid'
-import Leaf from '../Assets/leaf.png'
-import One from '../Assets/service-1-img-1.jpg'
-import Two from '../Assets/service-1-img-2.jpg'
-import Three from '../Assets/service-1-img-3.jpg'
-import Four from '../Assets/service-1-img-4.jpg'
+import React, { useState } from 'react';
+import { Container, Image } from 'react-bootstrap';
+import { v4 as uuid } from 'uuid';
+import Leaf from '../Assets/leaf.png';
 
 const Services = () => {
-  const boxes = [
+  const allBoxes = [
     {
       id: uuid(),
-      title: 'Fresh vegetables',
-      info: 'Lorem ipsum is simply are many variations of pass of majority.',
-      image: One,
+      title: 'Стабильный продовольственный рынок',
+      info: 'Обеспечение стабильного продовольственного рынка за счет устойчивых поставок, поддержки сельскохозяйственных производителей и предотвращения продовольственных кризисов.',
     },
     {
       id: uuid(),
-      title: 'Fresh vegetables',
-      info: 'Lorem ipsum is simply are many variations of pass of majority.',
-      image: Two,
+      title: 'Развитие сельского хозяйства',
+      info: 'Улучшение агротехнологий, внедрение инноваций и расширение поддержки фермеров для повышения урожайности и устойчивости сельского хозяйства.',
     },
+    {
+      id: uuid(),
+      title: 'Модернизация пищевой промышленности',
+      info: 'Внедрение современных технологий переработки и хранения продуктов, улучшение качества пищевых товаров и развитие экспортного потенциала отрасли.',
+    },
+    {
+      id: uuid(),
+      title: 'Поддержка сельхозпроизводителей',
+      info: 'Оказание финансовой и технической помощи фермерам, субсидирование сельхозпроизводства и создание благоприятных условий для устойчивого развития аграрного сектора.',
+    },
+    {
+      id: uuid(),
+      title: 'Развитие торгово-логистической инфраструктуры',
+      info: 'Создание современных складских комплексов, транспортных узлов и логистических центров для эффективного хранения, транспортировки и распределения сельскохозяйственной продукции.',
+    },
+    {
+      id: uuid(),
+      title: 'Экспортно-импортные операции',
+      info: 'Содействие выходу отечественной продукции на международные рынки, упрощение экспортных и импортных процедур, а также обеспечение выгодных условий торговли.',
+    },
+  ];
 
-    {
-      id: uuid(),
-      title: 'Fresh vegetables',
-      info: 'Lorem ipsum is simply are many variations of pass of majority.',
-      image: Three,
-    },
-    {
-      id: uuid(),
-      title: 'Fresh vegetables',
-      info: 'Lorem ipsum is simply are many variations of pass of majority.',
-      image: Four,
-    },
-  ]
+  const [visibleCount, setVisibleCount] = useState(3);
+  const isCollapsed = visibleCount === 3;
+
+  const toggleVisibility = () => {
+    setVisibleCount(isCollapsed ? allBoxes.length : 3);
+  };
+
   return (
-    <div className='services'>
+    <div className="services">
       <Container>
-        <div className='services__up'>
-          <p>What we do</p>
-          <h1>Services We Offer</h1>
-          <Image src={Leaf} alt='' />
+        <div className="services__up">
+          <div className="title-with-leaf">
+            <Image src={Leaf} alt="Leaf Icon" className="leaf-icon left"/>
+            <h1 className="section-title">Наши Цели</h1>
+            <Image src={Leaf} alt="Leaf Icon" className="leaf-icon right"/>
+          </div>
+          <p>
+            Открытое акционерное общество "Кыргыз Агрохолдинг" активно работает над развитием сельского хозяйства в
+            Кыргызстане. Мы стремимся улучшить аграрный сектор, поддерживая фермеров и развивая инновационные
+            технологии.
+          </p>
         </div>
-        <div className='services__down'>
-          {boxes?.map((box) => (
-            <div className='box' key={box.id}>
-              <div className='top'>
-                <h2>{box.title}</h2>
-                <p>{box.info}</p>
+
+        <div className="services__down">
+          <div className="grid-container">
+            {allBoxes.slice(0, visibleCount).map((box) => (
+              <div className="box" key={box.id}>
+                <div className="top">
+                  <h2>{box.title}</h2>
+                  <p>{box.info}</p>
+                </div>
               </div>
-              <div className='bottom'>
-                <Image src={box.image} alt='' />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
+          <div className="toggle-btn-container">
+            <button className="toggle-btn" onClick={toggleVisibility}>
+              {isCollapsed ? 'Показать больше' : 'Скрыть'}
+            </button>
+          </div>
         </div>
       </Container>
     </div>
-  )
-}
 
-export default Services
+
+  );
+};
+
+export default Services;
